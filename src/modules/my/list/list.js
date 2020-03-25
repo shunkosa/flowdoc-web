dd931130e0fac8205aa5a75c83574a769279db0d
shunkosa <shun.kosaka.102@gmail.com>
2020-03-25 11:59:35 +0900

    fix: render lists

--- src/modules/my/list/list.js
+++ src/modules/my/list/list.js
@@ -1,5 +1,4 @@
 import { LightningElement, track } from 'lwc';
-import JSForce from '../../../resources/jsforce';
 
 export default class List extends LightningElement {
     @track message = 'Loading...';
@@ -8,22 +7,47 @@ export default class List extends LightningElement {
 
     connectedCallback() {
         (async () => {
-            const jsforce = new JSForce(this.getCookie('instance_url'), this.getCookie('access_token'));
-            this.flowList = await jsforce.getFlowList();
-            this.isLoading = false;
+            const body = {
+                instance_url: decodeURIComponent(this.getCookie('instance_url')),
+                access_token: this.getCookie('access_token')
+            };
+            fetch('api/flows', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                },
+                body: JSON.stringify(body)
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    this.flowList = data.flows;
+                    const numOfAvailableFlows = this.flowList.filter((f) => f.isSupported).length;
+                    this.message = `${numOfAvailableFlows} of ${this.flowList.length} flows are available.`;
+                })
+                .catch((error) => console.log(error))
+                .finally(() => {
+                    this.isLoading = false;
+                });
         })();
     }
 
+    get hasFlowList() {
+        return this.flowList && Object.keys(this.flowList).length !== 0;
+    }
+
     getCookie(name) {
-        var value = "; " + document.cookie;
-        var parts = value.split("; " + name + "=");
-        if (parts.length === 2) { 
-            return parts.pop().split(";").shift();
+        var value = '; ' + document.cookie;
+        var parts = value.split('; ' + name + '=');
+        if (parts.length === 2) {
+            return parts.pop().split(';').shift();
         }
         return undefined;
     }
 
     chunk([...array], size = 1) {
-        return array.reduce((acc, value, index) => index % size ? acc : [...acc, array.slice(index, index + size)], []);
+        return array.reduce(
+            (acc, value, index) => (index % size ? acc : [...acc, array.slice(index, index + size)]),
+            []
+        );
     }
 }
